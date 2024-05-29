5e469d3332b9c81dc1b2723840b36edadd6cad67
Shakhzod Kudratov <shakhzodkudratov@gmail.com>
2024-05-29 20:22:38 +0500

    up to iterators

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(qty: i32) -> i32 {
+    if qty <= 40 {
+        qty * 2
+    } else {
+        qty
+    }
+}
 
 // Don't modify this function!
 #[test]
